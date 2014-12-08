80183200b188f0b79f2ed71921e7a70d499c73e5
bhagwan <bhagwan@datametica.com>
2014-12-08 21:16:37 +0530

    Transpose commit

---
+++ https:/github.com/BhagwanSoni/BectorTapu/udf/src/main/java/com/datametica/pig/udf/Transpose.java
@@ -0,0 +1,69 @@
+package com.datametica.pig.udf;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.StringTokenizer;
+
+import org.apache.pig.builtin.IsEmpty;
+
+class Transpose {
+	public static void main(String[] args) throws IOException {
+		File file = new File(
+				"/home/bhagwan/workspace/dataprofile/Transpose/target/file");
+		FileReader in = new FileReader(
+				"/home/bhagwan/workspace/dataprofile/Transpose/target/file_out");
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedReader br = new BufferedReader(in);
+		BufferedWriter bw = new BufferedWriter(fw);
+		String line = null;
+		int maxColumnCount = 0;
+		int i = 0, j = 0, rowCount = 0, columnCount = 0;
+		while ((line = br.readLine()) != null) {
+			columnCount = 0;
+			for (String word : line.split("\t")) {
+				++columnCount;
+			}
+			if (columnCount > maxColumnCount)
+				maxColumnCount = columnCount;
+			++rowCount;
+		}
+		in.close();
+		br.close();
+		String[][] table = new String[rowCount][maxColumnCount];
+		FileReader input;
+		try {
+			input = new FileReader(
+					"/home/bhagwan/workspace/dataprofile/Transpose/target/file_out");
+			BufferedReader br1 = new BufferedReader(input);
+
+			while ((line = br1.readLine()) != null) {
+				for (String word : line.split("\t")) {
+					table[i][j] = word;
+					++j;
+				}
+				j = 0;
+				++i;
+			} 
+			input.close();
+			br1.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+			
+		
+		for (j = 0; j < columnCount; j++) {
+			for (i = 0; i < rowCount; i++) {
+				bw.append(line);
+			}
+			bw.append("\n");
+		}
+		bw.close();
+	}
+
+}
